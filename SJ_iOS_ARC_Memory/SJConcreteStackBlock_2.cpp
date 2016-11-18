fc5832aef825a173aa2890428e1e10f688453624
zhoushejun <zhoushejun@passol.local>
2016-11-18 17:03:28 +0800

    优化注释。

--- SJ_iOS_ARC_Memory/SJConcreteStackBlock_2.cpp
+++ SJ_iOS_ARC_Memory/SJConcreteStackBlock_2.cpp
@@ -17,8 +17,8 @@ struct __block_impl {
   void *FuncPtr;
 };
 // Runtime copy/destroy helper functions (from Block_private.h)
-__OBJC_RW_DLLIMPORT void _Block_object_assign(void *, const void *, const int);
-__OBJC_RW_DLLIMPORT void _Block_object_dispose(const void *, const int);
+__OBJC_RW_DLLIMPORT void _Block_object_assign(void *, const void *, const int);//当 block 从栈拷贝到堆时，调用此函数。
+__OBJC_RW_DLLIMPORT void _Block_object_dispose(const void *, const int);//当 block 从堆内存释放时，调用此函数。
 __OBJC_RW_DLLIMPORT void *_NSConcreteGlobalBlock[32]; ///< 全局区 block
 __OBJC_RW_DLLIMPORT void *_NSConcreteStackBlock[32];  ///< 栈区 block
 
@@ -34,7 +34,7 @@ struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0 {
   struct __block_impl impl;//5.系统block 数据结构
   struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_desc_0* Desc;//6.1.MyBlock 的属性信息
   NSString *myStr;
-  __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0(void *fp, struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_desc_0 *desc, NSString *_myStr, int flags=0) : myStr(_myStr) {//构造函数
+  __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0(void *fp, struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_desc_0 *desc, NSString *_myStr, int flags=0) : myStr(_myStr) {//构造函数，多了一个NSString类型的参数:_myStr
     impl.isa = &_NSConcreteStackBlock;//7.isa指向它所属的类，取栈指针地址 --- 说明是在栈中分配的block
     impl.Flags = flags; //不传值时 默认值为0
     impl.FuncPtr = fp;  //MyBlock 的函数体指针
@@ -48,16 +48,17 @@ static void __SJConcreteStackBlock_2__concreteStackBlock_2_block_func_0(struct _
 
         NSLog(myStr);
     }
+/* copy : 将 栈上的 block 复制到 堆内存中 */
 static void __SJConcreteStackBlock_2__concreteStackBlock_2_block_copy_0(struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0*dst, struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0*src) {_Block_object_assign((void*)&dst->myStr, (void*)src->myStr, 3/*BLOCK_FIELD_IS_OBJECT*/);}
-
+/* dispose : 销毁 堆内存上的 block */
 static void __SJConcreteStackBlock_2__concreteStackBlock_2_block_dispose_0(struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0*src) {_Block_object_dispose((void*)src->myStr, 3/*BLOCK_FIELD_IS_OBJECT*/);}
 
 /* 6.MyBlock 的属性信息 */
 static struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_desc_0 {
   size_t reserved;
-  size_t Block_size;
-  void (*copy)(struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0*, struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0*);
-  void (*dispose)(struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0*);
+  size_t Block_size;//block 的字节大小
+  void (*copy)(struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0*, struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0*);//copy
+  void (*dispose)(struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0*);//dispose
 } __SJConcreteStackBlock_2__concreteStackBlock_2_block_desc_0_DATA = { 0, sizeof(struct __SJConcreteStackBlock_2__concreteStackBlock_2_block_impl_0), __SJConcreteStackBlock_2__concreteStackBlock_2_block_copy_0, __SJConcreteStackBlock_2__concreteStackBlock_2_block_dispose_0};
 
 /* 1.函数入口 */
